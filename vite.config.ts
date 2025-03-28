a3825f087e4862a28a8c18e39a87b2ace4115836
ariel <ferdmanariel707@gmail.com>
2025-03-29 00:02:51 +0100

    progress big

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    include: ['react-spring', '@use-gesture/react', 'react-tradingview-embed']
   },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  }
 });
